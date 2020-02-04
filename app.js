d9134eb03697f71af63a0e313f83fcba195450e6
Holly Phelps <Hollyphelps1992@gmail.com>
2020-02-03 20:44:36 -0800

    updates to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const Manager = require("./Manager");
+const Employee = require("./Employee");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+
+addManager(Manager) {
+    this.name.push(name);
+    this.id.push(id);
+    this.officNumber.push(officNumber);
+    
+    
+}
