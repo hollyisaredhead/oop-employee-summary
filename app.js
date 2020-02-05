5093ddbee034f2a66cec54bb9046d0ecfb7cb25d
Holly Phelps <Hollyphelps1992@gmail.com>
2020-02-04 21:25:35 -0800

    updates to app.js

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const Manager = require("./Manager");
-const Employee = require("./Employee");
-const Engineer = require("./Engineer");
-const Intern = require("./Intern");
+const fs = require('fs');
+const Manager = require('./Manager');
+const Employee = require('./Employee');
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
 
 addManager(Manager) {
     this.name.push(name);
     this.id.push(id);
     this.officNumber.push(officNumber);
-    
-    
-}
+}  
+
+
+fs.writeFile("employee.html", "Hey there", function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("File saved!")
+
+});
